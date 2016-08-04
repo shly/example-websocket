40a84ed68fd8e18d06d4cda323d802baee6bbe15
shly <1028442429@qq.com>
2016-08-04 16:58:30 +0800

    try

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var userName;
+
+/*访问静态资源*/
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',function(socket){
+	socket.on('disconnect',function(){
+		 io.emit('disconnect', 'a user disconnected');
+	});
+	socket.on('chat message', function(msg){
+    	 io.emit('chat message', userName+":"+msg);
+    });
+});
+
+
+http.listen(3000, function () {
+  console.log('Example app listening at 3000');
+});
+
